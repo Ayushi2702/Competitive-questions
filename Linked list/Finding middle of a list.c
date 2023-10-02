295eaf6aadd61448157c770b402ba6a3e7c6b032
Ayushi <ayushikarakoti02@gmail.com>
2023-10-02 09:18:38 +0530

    commit

--- Linked list/Finding middle of a list.c
+++ Linked list/Finding middle of a list.c
@@ -1,1 +1,67 @@
-fde
+#include <stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    /* data */
+    int data;
+    struct Node *next;
+}*first=NULL;
+void create(int A[],int n)
+{
+  int i;
+  struct Node *last,*t;
+  first=(struct Node *)malloc(sizeof(struct Node));
+  first->data=A[0];
+  first->next=NULL;
+  last=first;
+  for(i=1;i<n;i++)
+  {
+    t=(struct Node *)malloc(sizeof(struct Node));
+    t->data=A[i];
+    t->next=NULL;
+    last->next=t;
+    last=t;
+
+  }
+
+
+}
+
+void middle(struct Node *p)
+{
+    struct Node *q;
+    q=p;
+    while(q!=NULL)
+    {
+        q=q->next;
+        if(q){
+            q=q->next;
+        }
+        if(q){
+            p=p->next;
+        }
+    }
+    printf("%d\t",p->data);
+
+}
+
+
+
+void display(struct Node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d\t",p->data);
+        p=p->next;
+    }
+}
+
+int main()
+{
+    int A[]={1,2,3,4,5};
+    create(A,5);
+    display(first);
+    printf("The middle element of this list is :\n");
+    middle(first);
+    return 0;
+}
