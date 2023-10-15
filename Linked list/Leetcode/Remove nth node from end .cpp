1def7b7c0654bba039dc202752a43e58339942c4
Ayushi <ayushikarakoti02@gmail.com>
2023-10-15 17:26:21 +0530

    Medium

--- Linked list/Leetcode/Remove nth node from end .cpp
+++ Linked list/Leetcode/Remove nth node from end .cpp
@@ -1,30 +1,23 @@
-struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-    struct ListNode* start = (struct ListNode*)malloc(sizeof(struct ListNode));
-    start->next = head;
-    struct ListNode* fast = start;
-    struct ListNode* slow = start;
+class Solution {
+     public:
+ListNode* removeNthFromEnd(ListNode* head, int n){
+    ListNode* start=new ListNode();
+    start->next=head;
+    ListNode* fast=start;
+    ListNode* slow=start;
+    
+   for(int i=1;i<=n;i++)
+   {
+       fast=fast->next;
+   }
 
-    // Move fast pointer n nodes ahead
-    for(int i = 0; i < n; ++i) {
-        fast = fast->next;
-    }
+   while(fast->next!=NULL)
+   {
+       fast=fast->next;
+       slow=slow->next;
+   }
+   slow->next=slow->next->next;
+   return start->next;
 
-    // Move both pointers until fast reaches the end of the list
-    while(fast->next != NULL) {
-        fast = fast->next;
-        slow = slow->next;
-    }
-
-    // Remove the Nth node by updating the next pointer of the node before it
-    struct ListNode* temp = slow->next;
-    slow->next = slow->next->next;
-    free(temp);
-
-    // Get the updated head of the list (start->next points to the actual head)
-    struct ListNode* newHead = start->next;
-
-    // Free the dummy node
-    free(start);
-
-    return newHead;
 }
+ };
