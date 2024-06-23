af5f2f0f228fb2577318849536ee994024a91e2d
Ayushi <ayushikarakoti02@gmail.com>
2024-06-23 09:02:32 +0530

    commit

--- Stack/Leetcode/Valid Parathesis.java
+++ Stack/Leetcode/Valid Parathesis.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character>stack=new Stack<>();
          for(char c: s.toCharArray())
-         {
+         { 
            if(c=='(')
              stack.push(')');
 
