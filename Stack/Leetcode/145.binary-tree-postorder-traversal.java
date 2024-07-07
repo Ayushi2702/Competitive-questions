1c73a6e78968e80495ca957d78bc55f5a0e8dacd
Ayushi <ayushikarakoti02@gmail.com>
2024-07-07 10:38:18 +0530

    commit

---
+++ Stack/Leetcode/145.binary-tree-postorder-traversal.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=145 lang=java
+ *
+ * [145] Binary Tree Postorder Traversal
+ */
+
+// @lc code=start
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }                                  
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer>output = new LinkedList<>();
+
+Stack<TreeNode>stack = new Stack<>();
+if(root==null)
+{
+    return output;                            //left-right-root
+}
+     stack.push(root);
+         
+     while(!stack.isEmpty())
+     {
+        TreeNode current = stack.pop();
+
+        output.addFirst(current.val);
+        if(current.left!=null)
+        {
+            stack.push(current.left);
+        }
+
+        if(current.right!=null)
+        {
+            stack.push(current.right);
+        }
+     }
+
+     return output;
+
+        
+    }
+}
+// @lc code=end
+
