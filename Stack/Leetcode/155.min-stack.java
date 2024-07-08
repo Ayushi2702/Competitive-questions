e2b1fe8bc33d4ed995a751bf0a4fd832e347a470
Ayushi <ayushikarakoti02@gmail.com>
2024-07-08 13:59:23 +0530

    commit

--- Stack/Leetcode/155.min-stack.java
+++ Stack/Leetcode/155.min-stack.java
@@ -7,24 +7,40 @@
 // @lc code=start
 class MinStack {
 
+   private Stack<Integer> stack ;
+    private Stack<Integer>minStack ;
+
+
+
     public MinStack() {
-        
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
     
     public void push(int val) {
-        
+        stack.push(val);
+
+        if(minStack.isEmpty() || val<=minStack)
+        {
+            minStack.push(val);
+        }
     }
     
     public void pop() {
-        
+        if(stack.peek().equals(minStack.peek()))
+        {
+            minStack.pop();
+        }
+
+        stack.pop();
     }
     
     public int top() {
-        
+       return stack.peek();
     }
     
     public int getMin() {
-        
+        return minStack.peek();
     }
 }
 
