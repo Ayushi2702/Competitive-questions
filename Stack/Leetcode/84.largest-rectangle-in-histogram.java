8ea248df7cd7eaa7975d79eaba43ad0aad551b08
Ayushi <ayushikarakoti02@gmail.com>
2024-07-08 23:32:22 +0530

    "Added implementation for largest rectangle in histogram problem using stack"

--- Stack/Leetcode/84.largest-rectangle-in-histogram.java
+++ Stack/Leetcode/84.largest-rectangle-in-histogram.java
@@ -5,10 +5,30 @@
  */
 
 // @lc code=start
+import java.util.Stack;
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int maxArea = 0;
+        Stack<Integer> s = new Stack<>();
         
+        for (int i = 0; i <= n; i++) {
+            int currHeight = (i == n) ? 0 : heights[i];
+            
+            while (!s.isEmpty() && currHeight < heights[s.peek()]) {
+                int top = s.pop();
+                int width = s.isEmpty() ? i : i - s.peek() - 1;
+                int area = heights[top] * width;
+                maxArea = Math.max(area, maxArea);
+            }
+            
+            s.push(i);
+        }
+        
+        return maxArea;
     }
 }
+
 // @lc code=end
 
