d5ea99bc1df4f88456d6d67614113615ae3b93fa
Ayushi <ayushikarakoti02@gmail.com>
2024-07-10 22:29:42 +0530

    commit

--- Stack/Leetcode/225.implement-stack-using-queues.java
+++ Stack/Leetcode/225.implement-stack-using-queues.java
@@ -5,26 +5,54 @@
  */
 
 // @lc code=start
+
+import java.util.Queue;
+
 class MyStack {
+    
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
 
     public MyStack() {
         
+        queue1 = new LinkedList<Integer>();
+        queue2 = new LinkedList<Integer>();
     }
     
     public void push(int x) {
-        
+        queue2.add(val);
+        while(!queue1.isEmpty())
+        {
+            queue2.add(queue1.remove());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
     }
     
     public int pop() {
-        
+        if(!queue1.isEmpty())
+        {
+            return queue1.remove();
+        }
     }
     
     public int top() {
-        
+        if (!queue1.isEmpty()) {
+            
+            return queue1.peek();
+        }
     }
     
     public boolean empty() {
-        
+        if (queue1.isEmpty()) {
+             return false;
+
+        }
+        else{
+            return true;
+        }
     }
 }
 
