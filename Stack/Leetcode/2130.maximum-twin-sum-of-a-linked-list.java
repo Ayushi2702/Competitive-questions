8cf2df28207ac505968fe2a2d8d9f8198266ccb3
Ayushi <ayushikarakoti02@gmail.com>
2024-08-15 23:49:33 +0530

    commit empty

---
+++ Stack/Leetcode/2130.maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=2130 lang=java
+ *
+ * [2130] Maximum Twin Sum of a Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        
+    }
+}
+// @lc code=end
+
