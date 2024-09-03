8d4dd29939a9539d987841c76fc29fb67c0b058a
Ayushi <ayushikarakoti02@gmail.com>
2024-09-03 12:58:30 +0530

    commit

--- String/3.longest-substring-without-repeating-characters.java
+++ String/3.longest-substring-without-repeating-characters.java
@@ -5,9 +5,30 @@
  */
 
 // @lc code=start
+
+import java.util.Map;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
+    Map<Character, Integer> charIndexMap = new HashMap<>();
+
+    int maxLength =0;
+    int start=0;
+
+    for(int end=0;end<s.length();end++)
+    {
+        char current = s.charAt(end);
+
+        if(charIndexMap.containsKey(current) && charIndexMap.get(current)>=start)
+        {
+            start = charIndexMap.get(current) +1;
+        }
+
+        charIndexMap.put(current,end);
+        maxLength = Math.max(maxLength , end-start+1);
+    }
+
     }
 }
 // @lc code=end
