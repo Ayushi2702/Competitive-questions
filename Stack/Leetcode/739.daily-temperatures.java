558db8ce7ffce7838b55a103bbaaa915661de894
Ayushi <ayushikarakoti02@gmail.com>
2024-09-09 13:14:10 +0530

    commit

--- Stack/Leetcode/739.daily-temperatures.java
+++ Stack/Leetcode/739.daily-temperatures.java
@@ -7,7 +7,23 @@
 // @lc code=start
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        
+        int n = temperatures.length;
+        Stack<Integer> stack  = new Stack<>();
+        int[] result = new int[n];
+
+        for(int i =0;i<n;i++)
+        {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek(i)])
+            {
+                int index = stack.pop();
+                result[index] = i-index; 
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+
     }
 }
 // @lc code=end
