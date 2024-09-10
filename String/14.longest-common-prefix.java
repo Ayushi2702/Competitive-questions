03d5a9dd27bc30c0081e5415645084adfe153c90
Ayushi <ayushikarakoti02@gmail.com>
2024-09-10 09:52:25 +0530

    commit

--- String/14.longest-common-prefix.java
+++ String/14.longest-common-prefix.java
@@ -7,7 +7,27 @@
 // @lc code=start
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        
+        if(strs.length==0 || strs == null)
+        {
+            return "";
+        }
+
+        String prefix = strs[0];
+
+        for(int i=1;i<strs.length;i++)
+        {
+            while(strs[i].indexOf(prefix)!=0)
+            {
+                prefix = prefix.substring(0 , prefix.length() -1);
+            }
+
+            if(prefix.isEmpty())
+            {
+                return " ";
+            }
+        }
+
+        return prefix;
     }
 }
 // @lc code=end
