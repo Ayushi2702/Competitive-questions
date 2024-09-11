d9302b9dc5eb0c58ac8a41d532f7580cf521af30
Ayushi <ayushikarakoti02@gmail.com>
2024-09-11 10:13:35 +0530

    commit

--- Arrays/1.two-sum.java
+++ Arrays/1.two-sum.java
@@ -7,7 +7,24 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+             
+        Map<Integer, Integer>map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {  
+            int num = nums[i];
+            int complement = target - num;
+
+            if(map.containsKey(compelment))
+            {
+                return new int[] { map.get(complement) , i};
+            }
+
+            map.put(i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+
     }
 }
 // @lc code=end
