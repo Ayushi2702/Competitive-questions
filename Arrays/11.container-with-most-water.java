95765091456eff8f19b017ae0a4e51c62370f700
Ayushi <ayushikarakoti02@gmail.com>
2024-09-12 11:54:33 +0530

    commit

--- Arrays/11.container-with-most-water.java
+++ Arrays/11.container-with-most-water.java
@@ -7,7 +7,39 @@
 // @lc code=start
 class Solution {
     public int maxArea(int[] height) {
-        
+         if(height.length==0)
+         {
+            return 0;
+         }
+
+         int left = 0;
+         int right = height.length-1;
+         int maxArea = 0;
+
+         while(left<right)
+         {
+          int width = right - left;
+          int currentHeight = Math.min(left,right);
+
+          int area = width*currentHeight;
+
+          maxArea = Math.max(maxArea , area);
+
+          if(height[left] < height[right])
+          {
+            left++;
+          }
+
+          else{
+            right--;
+          }
+
+
+         }
+         
+     return maxArea;
+
+
     }
 }
 // @lc code=end
