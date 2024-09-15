e1278556819d93a4605117d371e58b2a4ad00f4d
Ayushi <ayushikarakoti02@gmail.com>
2024-09-15 23:53:08 +0530

    commit

--- Arrays/27.remove-element.java
+++ Arrays/27.remove-element.java
@@ -7,7 +7,22 @@
 // @lc code=start
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
+         
+        int k = 0;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+
+        return k;
+
+
+
     }
 }//
 // @lc code=end
