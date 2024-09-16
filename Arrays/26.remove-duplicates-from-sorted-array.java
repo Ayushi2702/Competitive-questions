48090520075f441f0455b8e6ecb5802edda8df8c
Ayushi <ayushikarakoti02@gmail.com>
2024-09-16 20:53:46 +0530

    commit

--- Arrays/26.remove-duplicates-from-sorted-array.java
+++ Arrays/26.remove-duplicates-from-sorted-array.java
@@ -8,6 +8,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         
+        if (nums.length == 0) return 0;
+
+        int unique = 0;
+
+        for(int i =1;i<nums.length;i++)
+        {
+            if(nums[i]!=nums[unique])
+            {
+                unique++;
+                nums[unique] = nums[i];
+            }
+        }
+
+        return unique+1;
     }
 }
 // @lc code=end
