ec75ba89c01032c7c8ebc60ba23b78b9ee23242b
Ayushi <ayushikarakoti02@gmail.com>
2024-09-25 23:12:19 +0530

    commit

--- Arrays/16.3-sum-closest.java
+++ Arrays/16.3-sum-closest.java
@@ -7,8 +7,42 @@
 // @lc code=start
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        
+        Arrays.sort(nums);
+        int closestSum = Integer.MAX_VALUE;
+
+        for(int i=0;i<nums.length-2;i++)
+        {
+            int left = i+1;
+            int right = nums.length-1;
+
+            while(left<right)
+            {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                if(Math.abs(currentSum - target) < Math.abs(closestSum - target))
+                {
+                    closestSum = currentSum;
+                }
+
+                if(currentSum < target)
+                {
+                    left++;
+                }
+
+                else if(currentSum > target)
+                {
+                    right--;
+                }
+
+
+                else{
+                    return currentSum;
+                }
+            }
+        }
+        return closestSum;
+
     }
-}//hi
+}
 // @lc code=end
 
